4e6f0da0b3dc1449fe60b76a5593d27547ef5a90
Tahmidul Alam <mohammed.alam.217@gmail.com>
2018-12-14 18:03:38 +0000

    v2

--- adventure.py
+++ adventure.py
@@ -1,24 +1,27 @@
 import sys
 import os
-import school.py, gym.py, restaurant.py
+# from school.py import School
+from gym import Gym
+# from restaurant.py import Restaurant
 
 def main():
-    user_name = input("Pick a Name: ")
+    print("Pick a Name: ", end="")
+    user_name = input()
     print("Hello, " + user_name + ".")
     print("Where would you like to go today?" + ": " )
     print("Your choices are: School, Gym, and Restaurant")
     choice = input()
 
     # User Choice
-    if choice.lower() == "school":
-        print("You are in school.")
-        school()
-    elif choice.lower() == "gym":
+    # if choice.lower() == "school":
+    #     print("You are in school.")
+    #     school()
+    if choice.lower() == "gym":
         print("Welcome to the Gym. Let's get hyped.")
-        gym()
-    elif choice.lower() == "restaurant":
-        print("You are feeling hungry. Time to eat!")
-        restaurant()
+        Gym()
+    # elif choice.lower() == "restaurant":
+    #     print("You are feeling hungry. Time to eat!")
+    #     restaurant()
 
 
 if __name__ == "__main__":
--- gym.py
+++ gym.py
@@ -1,3 +1,15 @@
 class Gym():
     def __init__(self, choice):
+        choice()
+    
+    def choice(self):
+        doneG = True
+        if doneG:
+            doneG = False
+            class_prompt = input("Do you want to run on the treadmill? (Y/N) :")
         
+        if class_prompt.lower() == "n":
+            class_prompt = input("Would you like to leave the gym? (Y/N) :")
+            
+        if class_prompt.lower() == "y":
+            print("You left gym.")
